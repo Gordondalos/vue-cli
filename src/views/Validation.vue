1072f553e5c422a16567a6bc53bccef01354abfd
Evgenii Bondarenko <gordondalos@gmail.com>
2018-12-09 16:50:09 +0600

    Валидация совпадение паролей

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -2,9 +2,11 @@
     <div>
         <div class="container text-left">
             <h1>Валидация</h1>
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <form>
+            <form>
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+
+
                         <label for="email">Email</label>
                         <input
                                 :class="{'is-invalid': $v.email.$error}"
@@ -17,40 +19,89 @@
                         <div class="invalid-feedback" v-if="!$v.email.email">
                             Не верная почта
                         </div>
-                    </form>
-                    <pre>
-                        {{$v.email}}
-                    </pre>
+
+
+                        <pre>
+                            {{$v.email}}
+                        </pre>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+
+                        <label for="password">Пароль</label>
+                        <input
+                                :class="{'is-invalid': $v.password.$error}"
+                                @blur="$v.password.$touch()"
+                                v-model="password" class="form-control" type="password" id="password">
+
+                        <div class="invalid-feedback" v-if="!$v.password.minLength">
+                            Минимальная длина {{$v.password.$params.minLength.min}} сейчас {{password.length}}
+                        </div>
+                        <div class="invalid-feedback" v-if="!$v.password.required">
+                            Поле обязательное
+                        </div>
+
+                        <pre>
+                            {{$v.password}}
+                        </pre>
+
+
+                        <label for="password2">Пароль</label>
+                        <input
+                                :class="{'is-invalid': $v.password2.$error}"
+                                @blur="$v.password2.$touch()"
+                                v-model="password2" class="form-control" type="password" id="password2">
+
+                        <div class="invalid-feedback" v-if="!$v.password2.sameAs">
+                            Пароль не совпадает
+                        </div>
+
+                        <pre>
+                            {{$v.password2}}
+                        </pre>
+
+
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
 
     </div>
 </template>
 
 <script>
-    import { required, email } from 'vuelidate/lib/validators'
+    import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 
     export default {
 
         data() {
             return {
                 msg: 'Да',
-                email: ''
+                email: '',
+                password: '',
+                password2: '',
             }
         },
         validations: {
             email: {
                 required,
                 email,
+            },
+            password: {
+                minLength: minLength( 6 ),
+                required,
+            },
+
+            password2: {
+                sameAs: sameAs( 'password' ),
+                required,
             }
         },
 
         mounted() {
-            console.log( this.msg );
-            _.each([1,2,3,4,5], (item) => {
-                console.log(item);
-            })
+            // тест лодеща
+            // _.each( [ 1, 2, 3, 4, 5 ], ( item ) => {
+            //     console.log( item );
+            // } )
         }
     }
 

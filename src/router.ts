772d8e3a91a6ae5afe095e613fa44a177927dfad
Evgenii Bondarenko <gordondalos@gmail.com>
2018-12-09 19:26:46 +0600

    Редиректы

--- src/router.ts
+++ src/router.ts
@@ -61,6 +61,19 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/none',
+      redirect: '/'
+    },
+
+    {
+      path: '/none',
+      redirect: 'carrFull'
+    },
+    {
+      path: '*',
+      redirect: '/car' // b
     }
   ]
 })

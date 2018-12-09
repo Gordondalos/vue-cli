62dcf5ea7270365fbee6d130c33e8bbaf812476f
Evgenii Bondarenko <gordondalos@gmail.com>
2018-12-09 10:36:36 +0600

    Аргументы и параметры директивы

--- src/directives/color.ts
+++ src/directives/color.ts
@@ -3,9 +3,12 @@ export default {
   // первый жизненный этап
   // при инициализации элемент  байдинг и втртуальная нода ( последние две только на чтение)
   bind(el: HTMLElement, bindings: any, vnode: any) {
-        el.style.color = '#2c6be0';
+        el.style.color = bindings.value; // bindings.value лежит передаваемый параметр тут передали грин
         console.log(bindings);
         console.log(vnode);
+        const arg = bindings.arg // тут лежат аргументы, типа беграунда
+
+        el.style[arg] = arg
   },
 
   // вставлена

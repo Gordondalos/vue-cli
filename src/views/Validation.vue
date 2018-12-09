01f40dbf26f2db67e3ec1a05434ab4af6709d293
Evgenii Bondarenko <gordondalos@gmail.com>
2018-12-09 16:31:57 +0600

    Валидация

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -6,8 +6,21 @@
                 <div class="col-sm-12 col-md-6">
                     <form>
                         <label for="email">Email</label>
-                        <input class="form-control" type="email" id="email">
+                        <input
+                                :class="{'is-invalid': $v.email.$error}"
+                                @blur="$v.email.$touch()"
+                                v-model="email" class="form-control" type="email" id="email">
+
+                        <div class="invalid-feedback" v-if="!$v.email.required">
+                            Полe обязательное.
+                        </div>
+                        <div class="invalid-feedback" v-if="!$v.email.email">
+                            Не верная почта
+                        </div>
                     </form>
+                    <pre>
+                        {{$v.email}}
+                    </pre>
                 </div>
             </div>
         </div>
@@ -16,11 +29,20 @@
 </template>
 
 <script>
+    import { required, email } from 'vuelidate/lib/validators'
+
     export default {
 
         data() {
             return {
-                msg: 'Да'
+                msg: 'Да',
+                email: ''
+            }
+        },
+        validations: {
+            email: {
+                required,
+                email,
             }
         },
 
